0575bcae1b14e0108d4b8f1dd7fcd76d2c4ddd08
1t0XX <53831070+1t0xx@users.noreply.github.com>
2020-04-30 14:42:56 +0300

    урок 6 ДЗ(звуки)

--- core/src/ru/geekbrains/screen/GameScreen.java
+++ core/src/ru/geekbrains/screen/GameScreen.java
@@ -1,6 +1,8 @@
 package ru.geekbrains.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -27,9 +29,14 @@ public class GameScreen extends BaseScreen {
     private Star[] starArray;
     private MainShip mainShip;
 
+    private Music music;
+
+
     @Override
     public void show() {
         super.show();
+        music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
+
         atlas = new TextureAtlas("textures/mainAtlas.tpack");
         bg = new Texture("textures/bg.png");
         background = new Background(new TextureRegion(bg));
@@ -39,6 +46,9 @@ public class GameScreen extends BaseScreen {
         }
         bulletPool = new BulletPool();
         mainShip = new MainShip(atlas, bulletPool);
+        music.setLooping(true);
+        music.play();
+
     }
 
     @Override
@@ -64,6 +74,8 @@ public class GameScreen extends BaseScreen {
         atlas.dispose();
         bg.dispose();
         bulletPool.dispose();
+        music.dispose();
+       mainShip.dispose();
         super.dispose();
     }
 
